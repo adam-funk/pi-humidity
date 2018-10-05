a5ea3272a5f32a40fa971d583f90c59fd197069a
Adam Funk <a.funk@sheffield.ac.uk>
2018-10-05 16:27:24 +0100

    starting

---
+++ log-temp-hum.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+
+import pigpio, DHT22, time
+import requests, json, datetime, time, argparse
+
+led_pin = 24
+sensor_pin = 23
+identifier = 'cellar'
+log_url = 'http://192.168.123.7:8089/data'
+local_file = '/home/pi/cellar.tsv'
+
+def signal(blinks, pause):
+    for i in range(0, blinks):
+        pi.write(led_pin, 1)
+        time.sleep(pause)
+        pi.write(led_pin, 0)
+        time.sleep(pause)
+    return
+
+def process(sensor, options):
+    sensor.trigger()
+    time.sleep(0.5)
+    now = datetime.datetime.now()
+    epoch = int(time.time())
+    iso_time = datetime.datetime.isoformat(now).split('.')[0]
+    t = round(sensor.temperature(), 1)
+    h = round(sensor.humidity())
+    if not options.quiet:
+        print(t, h, iso_time)
+    if t > 0 or h > 0:
+        signal(3, 0.5)
+    else:
+        signal(5, 0.3)
+    return (t, h, epoch, iso_time)
+
+def log(temperature, humidity, epoch, iso_time, options):
+    with open(local_file, 'a') as f:
+        f.write('%d\t%s\t%f\t%f\n' % (epoch, iso_time, temperature, humidity))
+    response = requests.post(log_url,
+                             json = {'id' : identifier,
+                                     'temperature' : temperature,
+                                     'humidity' : humidity,
+                                     'date_time' : iso_time,
+                                     'epoch' : epoch}
+    )
+    if not options.quiet:
+        print(response.status_code)
+        print(response.text)
+
+    if response.status_code != 200:
+        signal(20, 0.2)
+    return
+
+oparser = argparse.ArgumentParser(description="...",
+                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+oparser.add_argument("-q", dest="quiet", default=False,
+                     action='store_true',
+                     help="quiet")
+
+options = oparser.parse_args()
+
+
+pi = pigpio.pi()
+sensor = DHT22.sensor(pi, sensor_pin)
+
+t, h, epoch, iso_time = process(sensor, options)
+log(t, h, epoch, iso_time, options)
---
+++ plot-home-data.py
@@ -0,0 +1,49 @@
+import matplotlib, numpy
+import matplotlib.pyplot as plt
+from matplotlib import dates
+
+data_file = 'home-data.tsv'
+
+timestamps = []
+temperatures = []
+humidities = []
+
+days = dates.DayLocator(interval=1)
+daysFmt = dates.DateFormatter('%Y-%m-%d')
+
+with open(data_file, 'r') as f:
+    for line in f.readlines():
+        line_data = line.rstrip().split('\t')
+        epoch = int(line_data[0])
+        temp  = float(line_data[3])
+        hum   = float(line_data[4])
+        if (-10 < temp < 150) and (-1 < hum < 101):
+            nepoch = numpy.datetime64(epoch, 's')
+            timestamps.append(nepoch)
+            temperatures.append(temp)
+            humidities.append(hum)
+        else:
+            print("Rejected", epoch, temp, hum)
+        
+plt.ion()
+
+fig, ax = plt.subplots()
+
+ax.xaxis.set_major_locator(days)
+ax.xaxis.set_major_formatter(daysFmt)
+ax.format_xdata = daysFmt
+
+fig.autofmt_xdate()
+
+ax.plot(timestamps, temperatures, 'b,-',
+         timestamps, humidities, 'g,-')
+
+# https://matplotlib.org/gallery/text_labels_and_annotations/date.html
+# https://matplotlib.org/api/_as_gen/matplotlib.pyplot.subplots.html#matplotlib.pyplot.subplots
+# https://matplotlib.org/api/dates_api.html#matplotlib.dates.MonthLocator
+# https://matplotlib.org/api/_as_gen/matplotlib.pyplot.plot.html#matplotlib.pyplot.plot
+# https://matplotlib.org/tutorials/introductory/pyplot.html
+
+# TODO
+# max and min for each day
+# convert epoch to datetime then take ymd only
