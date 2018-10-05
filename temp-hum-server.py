64ac68c0f2bb4305559a8a452947a697df13c15d
Adam Funk <a.funk@sheffield.ac.uk>
2018-10-05 16:34:29 +0100

    starting

---
+++ temp-hum-server.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from bottle import route, run, post, request, static_file, response, template
+
+import datetime, json
+
+data_file = '/home/adam/home-data.tsv'
+
+@post('/data')
+def take_data():
+    j = request.json
+    print(j)
+
+    with open(data_file, 'a') as f:
+        line = '%s\t%s\t%s\t%s\t%s\n' % (j['epoch'], j['date_time'],
+                                         j['id'], j['temperature'], j['humidity'])
+        f.write(line)
+    return 'OK'
+
+run(host='0.0.0.0', port=8089, debug=True)
