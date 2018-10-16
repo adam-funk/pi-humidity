376becb31a05a19c09144dcaf9ae3a9ab34c32e6
Adam Funk <a.funk@sheffield.ac.uk>
2018-10-16 14:50:20 +0100

    using tinyDB

--- client-sensors.py
+++ client-sensors.py
@@ -2,24 +2,16 @@
 import argparse
 import datetime
 import json
-import os
-# TODO use tinyDB
 import time
 
 import pigpio
 import requests
+import tinydb
 
 import DHT22
 
 
-# led_pin = 24
-# sensor_pin = 23
-# identifier = 'cellar'
-# log_url = 'http://192.168.123.7:8089/data'
-# local_file = '/home/pi/cellar.tsv'
-
-
-def signal_blinks(led_pin, nbr_blinks, pause):
+def signal_blinks(led_pin, nbr_blinks, pause, pi):
     for i in range(0, nbr_blinks):
         pi.write(led_pin, 1)
         time.sleep(pause)
@@ -28,15 +20,7 @@ def signal_blinks(led_pin, nbr_blinks, pause):
     return
 
 
-def record_data(options, connection, epoch, iso, temp, hum, identifier):
-    cursor = connection.cursor()
-    cursor.execute('INSERT INTO data ( epoch, isotime, identifier, temp, hum, uploaded) '
-                   'VALUES (?, ?, ?, ?, ?, 0)', epoch, iso, identifier, temp, hum)
-    connection.commit()
-    return
-
-
-def process(sensor, led_pin, options, identifier):
+def get_data(sensor, led_pin, pi):
     sensor.trigger()
     time.sleep(0.5)
     now = datetime.datetime.now()
@@ -47,31 +31,55 @@ def process(sensor, led_pin, options, identifier):
     if not options.quiet:
         print(t, h, iso_time)
     if (t > -10) and (h > 0):
-        signal_blinks(led_pin, 3, 0.5)
+        signal_blinks(led_pin, 3, 0.5, pi)
     else:
-        signal_blinks(led_pin, 10, 0.2)
-    return t, h, epoch, iso_time
+        signal_blinks(led_pin, 10, 0.2, pi)
+    return {'epoch': epoch, 'temperature': t, 'humidity': h,
+            'date_time': iso_time, 'led_pin': led_pin}
 
 
-def log(temperature, humidity, epoch, iso_time, options, led_pin, log_url,
-        identifier, local_file):
+def record_locally(data_chunk, local_file, database):
     with open(local_file, 'a') as f:
-        f.write('%d\t%s\t%s\t%.1f\t%.f\n' % (epoch, iso_time, identifier, temperature, humidity))
-    response = requests.post(log_url,
-                             json={'id': identifier,
-                                   'temperature': temperature,
-                                   'humidity': humidity,
-                                   'date_time': iso_time,
-                                   'epoch': epoch}
-                             )
+        f.write('{epoch}\t{date_time}\t{identifier}\ttemperature\thumidity\n'.format_map(data_chunk))
+    database.insert(data_chunk)
+    return
+
+
+def post_data(data_chunk, log_url, pi):
+    response = requests.post(log_url, json=data_chunk)
     if not options.quiet:
         print(response.status_code)
         print(response.text)
-
+    led_pin = data_chunk['led_pin']
     if response.status_code == 200:
-        signal_blinks(led_pin, 6, 0.5)
-    else:
-        signal_blinks(led_pin, 20, 0.2)
+        signal_blinks(led_pin, 6, 0.5, pi)
+        return True
+    signal_blinks(led_pin, 20, 0.2, pi)
+    return False
+
+
+def main(options):
+    with open(options.config_file) as f:
+        config = json.load(f)
+    pi = pigpio.pi()
+    database = tinydb.TinyDB(options['database'])
+    tsv = config['local_file']
+
+    # Check each sensor and write results to the database.
+    for entry in config['sensors']:
+        sensor = DHT22.sensor(pi, entry['sensor_pin'])
+        led_pin = entry['led_pin']
+        data_chunk = get_data(sensor, led_pin, pi)
+        data_chunk['identifier'] = entry['identifier']
+        record_locally(data_chunk, tsv, database)
+
+    # Try to post each item in the database
+    for data_chunk in iter(database):
+        post_data(data_chunk, config['log_url'], pi)
+        database.remove(doc_ids=[data_chunk.doc_id])
+
+    if not options.quiet:
+        print('Outstanding data', len(database))
     return
 
 
@@ -88,13 +96,4 @@ oparser.add_argument("-c", dest="config_file",
 
 options = oparser.parse_args()
 
-with open(options.config_file) as f:
-    config = json.load(f)
-
-pi = pigpio.pi()
-
-for entry in config['sensors']:
-    sensor = DHT22.sensor(pi, entry['sensor_pin'])
-    led_pin = entry['led_pin']
-    t, h, epoch, iso_time = process(sensor, led_pin, options, entry['identifier'])
-    log(t, h, epoch, iso_time, options, led_pin, config['url'], entry['identifier'], config['local_file'])
+main(options)
--- server-sensors.py
+++ server-sensors.py
@@ -2,6 +2,7 @@
 
 import time
 
+import maya
 from bottle import post, request
 from bottledaemon import daemon_run
 
@@ -9,13 +10,15 @@ data_file = '/home/adam/sensors-data.tsv'
 
 pid_filename = '/tmp/sensors-server-%d.pid' % time.time()
 
-# TODO add timestamp to log file name to avoid clobbering
-log_filename = '/home/adam/sensors-server.log'
+# log file name includes timestamp to prevent clobbering
+log_filename = '/home/adam/sensors-server-%d.log' % round(time.time())
 
 
 @post('/data')
 def take_data():
     j = request.json
+    # print the ISO time (just to the second)
+    print(maya.now().iso8601().split('.')[0])
     print(j) # to the log?
 
     with open(data_file, 'a') as f:
@@ -23,7 +26,6 @@ def take_data():
                                          j['id'], j['temperature'], j['humidity'])
         f.write(line)
     return 'OK'
-
 
 # 0.0.0.0 makes it available on the LAN
 daemon_run(host='0.0.0.0', port=8089, pidfile=pid_filename, logfile=log_filename)
