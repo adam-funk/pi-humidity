999597f89ef7b3a559f8caf20568feefe0577add
Adam Funk <a.funk@sheffield.ac.uk>
2018-10-16 15:38:45 +0100

    config

--- server-sensors.py
+++ server-sensors.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python3
 
+import json
+import os
 import time
 
 import maya
 from bottle import post, request
 from bottledaemon import daemon_run
-
-data_file = '/home/adam/sensors-data.tsv'
-
-pid_filename = '/tmp/sensors-server.pid'
-
-# log file name includes timestamp to prevent clobbering
-log_filename = '/home/adam/sensors-server-%d.log' % round(time.time())
 
 
 @post('/data')
@@ -21,11 +16,26 @@ def take_data():
     print(maya.now().iso8601().split('.')[0])
     print(j)
 
-    with open(data_file, 'a') as f:
+    with open(data_filename, 'a') as f:
         line = '%s\t%s\t%s\t%s\t%s\n' % (j['epoch'], j['date_time'],
                                          j['identifier'], j['temperature'], j['humidity'])
         f.write(line)
     return 'OK'
 
+
+HERE=os.path.dirname(os.path.realpath(__file__))
+
+with open(os.path.join(HERE, 'server.json')) as f:
+    config = json.load(f)
+
+directory = config['directory']
+data_filename = os.path.join(directory, config['data_file'])
+
+pid_filename = '/tmp/sensors-server.pid'
+
+# log file name includes timestamp to prevent clobbering
+log_filename = os.path.join(directory, 'sensors-server-%d.log' % round(time.time()))
+
 # 0.0.0.0 makes it available on the LAN
-daemon_run(host='0.0.0.0', port=8089, pidfile=pid_filename, logfile=log_filename)
+daemon_run(host='0.0.0.0', port=config['port'],
+           pidfile=pid_filename, logfile=log_filename)
