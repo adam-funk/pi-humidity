a44bdf94407cc834857e8986890388bbc1904fcf
Adam Funk <a.funk@ducksburg.com>
2021-11-26 18:09:27 +0000

    WIP local data

--- record.py
+++ record.py
@@ -8,6 +8,7 @@ import pigpio
 import requests
 
 import DHT22
+import sensorutils
 
 
 def signal_blinks(led_pin, nbr_blinks, pause, pi):
@@ -24,93 +25,40 @@ def get_data(sensor, led_pin, pi):
     time.sleep(0.5)
     now = datetime.datetime.now()
     epoch = int(time.time())
-    iso_time = datetime.datetime.isoformat(now).split('.')[0]
-    t = round(sensor.temperature(), 1)
-    h = round(sensor.humidity())  # this returns int
-    if not options.quiet:
-        print('Measurement:', iso_time, t, h)
-    if (t > -10) and (h > 0):
+    temperature = round(sensor.temperature(), 1)
+    humidity = round(sensor.humidity(), 1)
+    if options.verbose:
+        iso_time = datetime.datetime.isoformat(now).split('.')[0]
+        print('Measurement:', iso_time, temperature, humidity)
+    if (temperature > -10) and (humidity > 0):
         signal_blinks(led_pin, 3, 0.5, pi)
     else:
         signal_blinks(led_pin, 9, 0.2, pi)
-    return {'epoch': epoch, 'temperature': t, 'humidity': h,
-            'date_time': iso_time, 'led_pin': led_pin}
+    return epoch, temperature, humidity, now
 
 
-def record_locally(data_chunk, local_file, database):
-    with open(local_file, 'a') as f:
-        f.write('{epoch}\t{date_time}\t{identifier}\t{temperature}\t{humidity}\n'.format_map(data_chunk))
-    database.insert(data_chunk)
-    return
-
-
-def post_data(data_chunk, log_url, pi):
-    success = False
-    led_pin = data_chunk['led_pin']
-    if not options.quiet:
-        print('Posting:', data_chunk['date_time'])
-    try:
-        response = requests.post(log_url, json=data_chunk)
-        if not options.quiet:
-            print(response.status_code)
-            print(response.text)
-        if response.status_code == 200:
-            success = True
-    except Exception as e:
-        if not options.quiet:
-            print(str(e))
-    if success:
-        signal_blinks(led_pin, 6, 0.5, pi)
-    else:
-        signal_blinks(led_pin, 15, 0.2, pi)
-    return success
-
-
-def main(options, start_time):
-    with open(options.config_file) as f:
+def main(options1):
+    with open(options1.config_file) as f:
         config = json.load(f)
     pi = pigpio.pi()
-    database = tinydb.TinyDB(config['database'])
-    tsv = config['local_file']
+    data_location = sensorutils.DataLocation(config['data_directory'])
 
-    if config.get('time_limit'):
-        cutoff_time = start_time + config['time_limit']
-    else:
-        cutoff_time = None
-
-    # Check each sensor and write results to the database.
+    # Check each sensor and write results to the file.
     for entry in config['sensors']:
         sensor = DHT22.sensor(pi, entry['sensor_pin'])
         led_pin = entry['led_pin']
-        data_chunk = get_data(sensor, led_pin, pi)
-        data_chunk['identifier'] = entry['identifier']
-        record_locally(data_chunk, tsv, database)
-
-    # Try to post each item in the database but quit trying
-    # after one fails or when the time limit is reached.
-    # Delete each one when successful.
-    # The database contains only unPOSTed data.
-    for data_chunk in iter(database):
-        if cutoff_time and (time.time() > cutoff_time):
-            if not options.quiet:
-                print('Cutoff time reached')
-            break
-        elif post_data(data_chunk, config['url'], pi):
-            database.remove(doc_ids=[data_chunk.doc_id])
-        else:
-            break
-
-    if not options.quiet:
-        print('Outstanding data', len(database))
+        location = entry['location']
+        epoch, temperature, humidity, now = get_data(sensor, led_pin, pi)
+        data_location.record(epoch, now, location, temperature, humidity)
     return
 
 
 oparser = argparse.ArgumentParser(description="Client for temperature logging",
                                   formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-oparser.add_argument("-q", dest="quiet", default=False,
+oparser.add_argument("-v", dest="verbose", default=False,
                      action='store_true',
-                     help="quiet")
+                     help="verbose")
 
 oparser.add_argument("-c", dest="config_file",
                      metavar="JSON",
@@ -118,4 +66,4 @@ oparser.add_argument("-c", dest="config_file",
 
 options = oparser.parse_args()
 
-main(options, int(time.time()))
+main(options)
--- sensorutils.py
+++ sensorutils.py
@@ -1,11 +1,12 @@
 # common code for handling the data directory
 
 # filename format:
-# DATA_DIR/sensors-yyyy-mm-dd.csv
+# DATA_DIR/sensors-yyyy-mm-dd.tsv
 
 # file format:
 # epoch, date_time, location, temperature, humidity
 import datetime
+import os.path
 
 
 class DataLocation:
@@ -13,4 +14,25 @@ class DataLocation:
     def __init__(self, data_directory):
         self.directory = data_directory
 
-    def get_file(self, date: datetime.date):
+    def get_filename(self, date: datetime.date):
+        """
+        Generate the correct filename for the given date.
+        """
+        date_string = date.isoformat()
+        filename = os.path.join(self.directory, f'sensors{date_string}.tsv')
+        return filename
+
+    def find_files(self, max_days_ago: int):
+        """
+        Search the directory for data files up to X days ago.
+        """
+        date_range = [datetime.date.today() - datetime.timedelta(days=d) for d in range(0, max_days_ago)]
+        look_for = [self.get_filename(d) for d in date_range]
+        return [filename for filename in look_for if os.path.exists(filename)]
+
+    def record(self, epoch: int, date_time: datetime.datetime, location:str, temperature: float, humidity: float):
+        filename = self.get_filename(date_time.date())
+        iso_time = datetime.datetime.isoformat(date_time).split('.')[0]
+        with open(filename, 'a', encoding='utf-8') as f:
+            f.write(f'{epoch}\t{iso_time}\t{location}\t{temperature}\t{humidity}\n')
+        return
