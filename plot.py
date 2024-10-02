41e13f07b8bba80b1f82dec6fdf7e594aa9e2c04
Adam Funk <a.funk@ducksburg.com>
2024-10-02 11:17:53 +0100

    updating pd

--- plot.py
+++ plot.py
@@ -136,7 +136,7 @@ def generate_plots(dataframe0: pd.DataFrame, config1: dict, verbose: bool):
     if verbose:
         print('Smoothed df', averaged.shape)
 
-    columns = ['min', 'meanr', 'medianr', 'max']
+    columns = ['min', meanr, medianr, 'max']
     dated = dataframe0.groupby('date').agg({'temperature': columns, 'humidity': columns,
                                             'pressure': columns, 'resistance': columns}).rename(
         columns={'meanr': 'mean', 'medianr': 'mdn'})
